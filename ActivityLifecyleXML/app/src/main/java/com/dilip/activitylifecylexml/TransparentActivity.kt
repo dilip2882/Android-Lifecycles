02a25763b6c79714c3c3ad2720ed086e3a23e12c
Dilip Patel <pateldilip2902@gmail.com>
2024-11-21 13:02:25 +0530

    refactor ActivityLifecycle

---
+++ ActivityLifecyleXML/app/src/main/java/com/dilip/activitylifecylexml/TransparentActivity.kt
@@ -0,0 +1,72 @@
+package com.dilip.activitylifecylexml
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
+
+class TransparentActivity : AppCompatActivity() {
+
+    private lateinit var backgroundDetector: BackgroundDetector
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.i("onCreate()")
+        Toast.makeText(this, "TransparentActivity - onCreate", Toast.LENGTH_SHORT).show()
+
+        val application = this.application
+
+        super.onCreate(savedInstanceState)
+
+        backgroundDetector = (application as CustomApplication).backgroundDetector
+
+        setContentView(R.layout.activity_transparent)
+    }
+
+    override fun onDestroy() {
+        Timber.i("onDestroy()")
+        Toast.makeText(this, "TransparentActivity - onDestroy", Toast.LENGTH_SHORT).show()
+        super.onDestroy()
+    }
+
+    override fun onStart() {
+        Timber.i("onStart()")
+        Toast.makeText(this, "TransparentActivity - onStart", Toast.LENGTH_SHORT).show()
+        super.onStart()
+        backgroundDetector.activityStarted()
+    }
+
+    override fun onStop() {
+        Timber.i("onStop()")
+        Toast.makeText(this, "TransparentActivity - onStop", Toast.LENGTH_SHORT).show()
+        super.onStop()
+        backgroundDetector.activityStopped()
+    }
+
+    override fun onResume() {
+        Timber.i("onResume()")
+        Toast.makeText(this, "TransparentActivity - onResume", Toast.LENGTH_SHORT).show()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Timber.i("onPause()")
+        Toast.makeText(this, "TransparentActivity - onPause", Toast.LENGTH_SHORT).show()
+        super.onPause()
+    }
+
+    override fun onTopResumedActivityChanged(isTopResumedActivity: Boolean) {
+        super.onTopResumedActivityChanged(isTopResumedActivity)
+        Timber.i("onTopResumedActivityChanged(); isTopResumed: $isTopResumedActivity")
+        Toast.makeText(this, "TransparentActivity - onTopResumedActivityChanged: $isTopResumedActivity", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        @JvmStatic
+        fun start(context: Context) {
+            val intent = Intent(context, TransparentActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+}
